276f168b419c144080e3d113f556bf207e2439e6
sanay3435 <sanaysri34@gmail.com>
2026-05-26 15:04:31 +0530

    Update singing.py

--- test/singing.py
+++ test/singing.py
@@ -2,17 +2,19 @@ import numpy as np
 import sounddevice as sd
 import librosa
 import time
+import keyboard
 
 SAMPLE_RATE = 16000
 CHUNK_SIZE = 2048
 
+current_mode = 'single'
 current_pitch = None
 current_confidence = 0.0
 current_volume = 0.0
 
 
 def detect_pitch(audio_chunk):
-  
+
 
     f0, voiced_flag, voiced_probs = librosa.pyin(
         audio_chunk,
@@ -51,6 +53,36 @@ def frequency_to_note(frequency):
 
     return f"{note_name}{octave}"
 
+def harmony(root,mode,octave):
+    notesref = [
+        "C", "C#", "D", "D#", "E", "F",
+        "F#", "G", "G#", "A", "A#", "B"
+    ]
+    root = notesref.index(root[:-1]) + 1
+    notes = [root]
+    output = []
+    if mode == 'single':
+        pass
+    if mode == 'major':
+        notes.append(root + 4)
+        notes.append(root + 7)
+    if mode =='minor':
+        notes.append(root + 3)
+        notes.append(root + 7)
+    for i in notes:
+        midi = (i%12)-1
+        key = notesref[midi]
+        output.append(key + octave)
+    return output
+
+def harmony_mode():
+    global current_mode
+    if keyboard.is_pressed('1'):
+        current_mode ='major'
+    if keyboard.is_pressed('2'):
+        current_mode ='minor'
+    if keyboard.is_pressed('0'):
+        current_mode='single'
 
 def audio_callback(indata, frames, time_info, status):
 
@@ -64,13 +96,11 @@ def audio_callback(indata, frames, time_info, status):
 
     audio_chunk = indata[:, 0]
 
-    
+   
     current_volume = np.sqrt(np.mean(audio_chunk ** 2))
 
- 
     if current_volume < 0.01:
         return
-
 
     pitch, confidence = detect_pitch(audio_chunk)
 
@@ -82,8 +112,8 @@ def audio_callback(indata, frames, time_info, status):
 print("Starting pitch detection...")
 print("Press Ctrl+C to stop\n")
 
-print(" Frequency | Note | Confidence | Volume ")
-print("----------------------------------------")
+print(" Frequency | Note | Confidence | Volume | Harmony | Output")
+print("----------------------------------------------------------")
 
 try:
     with sd.InputStream(
@@ -94,16 +124,18 @@ try:
     ):
 
         while True:
-
+            harmony_mode()
             if current_pitch is not None:
 
                 note = frequency_to_note(current_pitch)
-
+                
                 print(
                     f"{current_pitch:10.2f} Hz | "
                     f"{note:5} | "
                     f"{current_confidence:.3f} | "
-                    f"{current_volume:.4f}"
+                    f"{current_volume:.4f} | "
+                    f"{current_mode} | "
+                    f"{harmony(note,current_mode,note[-1])} | "
                 )
 
             time.sleep(0.01)
