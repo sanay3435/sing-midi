b10a18476a7a0b707283cd4d23e2c49dcb254a0c
sanay3435 <sanaysri34@gmail.com>
2026-06-07 16:44:12 +0530

    Update hysteresis.cpp

--- filters/hysteresis.cpp
+++ filters/hysteresis.cpp
@@ -1,1 +1,18 @@
+#include "hysteresis.h"
 
+int Hysteresis::apply(float midi_float)
+{
+    if (last_midi < 0)
+        last_midi = (int)(midi_float + 0.5f);
+
+    float lower = last_midi - 0.35f;
+    float upper = last_midi + 0.65f;
+
+    if (midi_float > upper)
+        last_midi++;
+
+    else if (midi_float < lower)
+        last_midi--;
+
+    return last_midi;
+}
